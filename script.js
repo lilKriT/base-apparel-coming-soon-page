adf85aaeab9e117338449926e90f1ff1d1bce4c2
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-07 23:25:43 +0200

    Added validation method.

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
 const form = document.querySelector("#subscribe-now");
 const emailField = document.querySelector("#email");
+
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function isValid(field, regex) {
+  return String(field).toLowerCase().match(regex);
+}
