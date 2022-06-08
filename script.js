bb346eebc33aff64d8bdac78e57a1374c20f4a11
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-08 18:53:31 +0200

    Added a listener.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// alert("Script works");
-
 const form = document.querySelector("#subscribe-now");
 const emailField = document.querySelector("#email");
 
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+form.addEventListener("submit", (e) => {
+  alert("Sending");
+})
 
 function isValid(field, regex) {
   return String(field).toLowerCase().match(regex);
